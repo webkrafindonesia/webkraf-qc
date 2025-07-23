ebdd37edbae2acde409effe72596384e04002c90
Willi Kristianto <willi.kristianto@webkraf.com>
2025-07-23 11:37:28 +0700

    FORCE_HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
+use TomatoPHP\FilamentUsers\Facades\FilamentUser;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,14 @@ class AppServiceProvider extends ServiceProvider
         FilamentColor::register([
             //'danger' => Color::hex('#00F'), // Example custom color
         ]);
+
+        FilamentUser::register([
+            \Filament\Resources\RelationManagers\RelationManager::make() // Replace with your custom relation manager
+        ]);
+
+        if(env('FORCE_HTTPS', false)){
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
